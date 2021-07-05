8c5c196c0d67a11268aa947eaa23c93d33c2cc12
Edgar Herrera <edgarahg13@hotmail.com>
2021-07-05 17:09:49 -0500

    updateVariantHMM2

--- variants/meow_meow2/variant.h
+++ variants/meow_meow2/variant.h
@@ -64,9 +64,9 @@
 // ----
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (27u)
-#define NUM_DIGITAL_PINS     (13u)
-#define NUM_ANALOG_INPUTS    (11u)
+#define PINS_COUNT           (40u)
+#define NUM_DIGITAL_PINS     (36u)
+#define NUM_ANALOG_INPUTS    (4u)
 #define NUM_ANALOG_OUTPUTS   (1u)
 
 // Low-level pin register query macros
@@ -92,39 +92,44 @@
 
 // LEDs
 // ----
-#define PIN_LED_13  (13u)
-#define PIN_LED     PIN_LED_13
-#define LED_BUILTIN PIN_LED
+#define PIN_LED_23  (23u)
+#define LED_BUILTIN PIN_LED_23
 
 /*
  * Analog pins
  */
-#define PIN_A0               (14ul)
-#define PIN_A1               (PIN_A0 + 1)
-#define PIN_A2               (PIN_A0 + 2)
-#define PIN_A3               (PIN_A0 + 3)
-#define PIN_A4               (PIN_A0 + 4)
-#define PIN_A5               (PIN_A0 + 5)
-#define PIN_A6               (PIN_A0 + 6)
-#define PIN_A7               (PIN_A0 + 7)
-#define PIN_A8               (PIN_A0 + 8)
-#define PIN_A9               (PIN_A0 + 9)
-#define PIN_A10              (PIN_A0 + 10)
+#define PIN_Y0               (2ul)
+#define PIN_Y1               (3ul)
+#define PIN_Y2               (4ul)
+#define PIN_Y3               (5ul)
+#define PIN_Y4               (6ul)
+#define PIN_Y5               (7ul)
+#define PIN_A3               (8ul)
+#define PIN_A2               (9ul)
+#define PIN_A1              (10ul)
+#define PIN_A0              (11ul)
+#define PIN_Y8              (32ul)
+#define PIN_Y9              (33ul)
+#define PIN_Y14             (34ul)
+#define PIN_Y15             (35ul)
 
-#define PIN_DAC0             (PIN_A0)
+#define PIN_DAC0            (PIN_Y1)
 
-
-static const uint8_t A0  = PIN_A0;
-static const uint8_t A1  = PIN_A1;
-static const uint8_t A2  = PIN_A2;
+static const uint8_t Y0  = PIN_Y0;
+static const uint8_t Y1  = PIN_Y1;
+static const uint8_t Y2  = PIN_Y2;
+static const uint8_t Y3  = PIN_Y3;
+static const uint8_t Y4  = PIN_Y4;
+static const uint8_t Y5  = PIN_Y5;
 static const uint8_t A3  = PIN_A3;
-static const uint8_t A4  = PIN_A4;
-static const uint8_t A5  = PIN_A5;
-static const uint8_t A6  = PIN_A6 ;
-static const uint8_t A7  = PIN_A7 ;
-static const uint8_t A8  = PIN_A8 ;
-static const uint8_t A9  = PIN_A9 ;
-static const uint8_t A10  = PIN_A10 ;
+static const uint8_t A2  = PIN_A2;
+static const uint8_t A1  = PIN_A1;
+static const uint8_t A0  = PIN_A0;
+static const uint8_t Y8  = PIN_Y8;
+static const uint8_t Y9  = PIN_Y9;
+static const uint8_t Y14  = PIN_Y14;
+static const uint8_t Y15  = PIN_Y15;
+
 static const uint8_t DAC0 = PIN_DAC0;
 
 #define ADC_RESOLUTION		12
@@ -137,14 +142,13 @@ static const uint8_t DAC0 = PIN_DAC0;
  */
 #define SPI_INTERFACES_COUNT 1
 
-#define PIN_SPI_MISO         (8u)
-#define PIN_SPI_SCK          (9u)
-#define PIN_SPI_MOSI         (10u)
+#define PIN_SPI_MISO         (22u)
+#define PIN_SPI_SCK          (19u)
+#define PIN_SPI_MOSI         (18u)
 #define PERIPH_SPI           sercom3
-#define PAD_SPI_TX           SPI_PAD_2_SCK_3  // MOSI / SCK
-#define PAD_SPI_RX           SERCOM_RX_PAD_0  // MISO
+#define PAD_SPI_TX           SPI_PAD_0_SCK_2  // MOSI / SCK
+#define PAD_SPI_RX           SERCOM_RX_PAD_1  // MISO
 
-static const uint8_t SS	  = 7u ;
 static const uint8_t MOSI = PIN_SPI_MOSI ;
 static const uint8_t MISO = PIN_SPI_MISO ;
 static const uint8_t SCK  = PIN_SPI_SCK ;
@@ -155,8 +159,8 @@ static const uint8_t SCK  = PIN_SPI_SCK ;
 #define WIRE_INTERFACES_COUNT 1
 
 // "external" public i2c interface
-#define PIN_WIRE_SDA         (11u)
-#define PIN_WIRE_SCL         (12u)
+#define PIN_WIRE_SDA         (0u)
+#define PIN_WIRE_SCL         (1u)
 #define PERIPH_WIRE          sercom1
 #define WIRE_IT_HANDLER      SERCOM1_Handler
 static const uint8_t SDA = PIN_WIRE_SDA;
@@ -166,8 +170,8 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 // ---
 //#define PIN_USB_HOST_ENABLE (25ul)
 #define PIN_USB_HOST_ENABLE_VALUE	0
-#define PIN_USB_DM          (26ul)
-#define PIN_USB_DP          (27ul)
+#define PIN_USB_DM          (24ul)
+#define PIN_USB_DP          (25ul)
 
 // I2S Interfaces
 // --------------
@@ -189,8 +193,8 @@ extern SERCOM sercom5;
 
 // Serial1
 extern Uart Serial1;
-#define PIN_SERIAL1_RX       (0ul)
-#define PIN_SERIAL1_TX       (1ul)
+#define PIN_SERIAL1_RX       (13ul)
+#define PIN_SERIAL1_TX       (12ul)
 #define PAD_SERIAL1_TX       (UART_TX_PAD_0)
 #define PAD_SERIAL1_RX       (SERCOM_RX_PAD_1)
 #endif // __cplusplus
